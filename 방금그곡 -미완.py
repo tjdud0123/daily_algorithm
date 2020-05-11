07c2e2e02aac613d0125febaa0f7be40a123c1f5
Um seoyoung <tjdud0123@naver.com>
2020-05-12 02:49:59 +0900

    방금그곡 완성

--- 방금그곡 -미완.py
+++ 방금그곡 -미완.py
@@ -1,14 +1,18 @@
 def solution(m, musicinfos):
-    music_dic = {}
     LEN = len(m)
-    for music in musicinfos:
+    cand = []
+    m = m.replace('C#', 'c').replace('D#', 'd').replace('F#', 'f')\
+        .replace('G#', 'g').replace('A#', 'a')
+
+    for i, music in enumerate(musicinfos):
+        music = music.replace('C#', 'c').replace('D#', 'd').replace('F#', 'f')\
+            .replace('G#', 'g').replace('A#', 'a')
         start, end, song, scale = music.split(',')
         s_hour, s_minute = map(int, start.split(':'))
         e_hour, e_minute = map(int, end.split(':'))
         play = (e_hour*60 + e_minute) - (s_hour*60 + s_minute)
         played = (scale*LEN)[:play]
         if m in played:
-            is_shrp = played.index(m)+LEN
-            if len(played)-1 < is_shrp or played[is_shrp] != '#':
-                return song
-    return None
+            cand.append((play, i, song))
+    cand = sorted(cand, key=lambda x: (-x[0], x[1]))
+    return "(None)" if not cand else cand[0][2]
