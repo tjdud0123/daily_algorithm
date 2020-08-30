e05c0b20cf3c6af4e59383c1e7ca05d796da7585
Um seoyoung <tjdud0123@naver.com>
2020-08-31 01:08:32 +0900

    자물쇠와 열쇠 다른풀이 수정

--- 카카오블라인드2020/3번_자물쇠와 열쇠.py
+++ 카카오블라인드2020/3번_자물쇠와 열쇠.py
@@ -1,9 +1,9 @@
-# 다른 풀이
+# 다른 풀이 - 보드크기를 세배로
 def rotate90(a):
     n = len(a)
     m = len(a[0])
 
-    result = [[0] * n for _in range(m)]
+    result = [[0] * n for _ in range(m)]
     for i in range(n):
         for j in range(m):
             result[j][n-1-i] = a[i][j]
@@ -21,9 +21,27 @@ def check(new_lock):
 
 
 def solution(key, lock):
-    n, m = len(key), len(lock)
+    n, m = len(lock), len(key)
 
-    new_lock = [[0] * (n*3) for _in range(n*3)]
+    new_lock = [[0] * (n*3) for _ in range(n*3)]
     for i in range(n):
         for j in range(n):
-            new_lock[][]
+            new_lock[i+n][j+n] = lock[i][j]
+
+    for rotation in range(4):
+        key = rotate90(key)
+        for x in range(n*2):
+            for y in range(n*2):
+
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x+i][y+j] += key[i][j]
+
+                if check(new_lock):
+                    return True
+
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x+i][y+j] -= key[i][j]
+
+    return False
