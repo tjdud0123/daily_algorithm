2a442b8a8714468e56a282ae12dca1c6ad824734
Um seoyoung <tjdud0123@naver.com>
2020-08-31 02:58:27 +0900

    외벽점검 두분째 풀이

--- 카카오블라인드2020/4번_외벽점검.py
+++ 카카오블라인드2020/4번_외벽점검.py
@@ -1,3 +1,33 @@
+# 풀이 1 - permutation 이용
+from itertools import permutations
+
+
+def solution(n, weak, dist):
+    L = len(weak)
+    cand = []
+    weak_point = weak + [w+n for w in weak]  # 선형으로
+
+    for i, start in enumerate(weak):
+        for friends in permutations(dist):  # 순열 이용
+            count = 1
+            position = start
+            # 친구 조합 배치
+            for friend in friends:
+                position += friend
+                # 끝 포인트까지 도달 못했을 때
+                if position < weak_point[i+L-1]:
+                    count += 1  # 친구 더 투입
+                    # 현재 위치보다 멀리 있는 취약지점 중 가장 가까운 위치로
+                    position = [w for w in weak_point[i+1:i+L]
+                                if w > position][0]
+                else:  # 끝 포인트까지 도달
+                    cand.append(count)
+                    break
+
+    return min(cand) if cand else -1
+
+
+#  풀이 2 - set 이용
 def solution(n, weak, dist):
 
     W, F = len(weak), len(dist)
