07824461298d78476201071e39a60ea6482a540b
Um seoyoung <tjdud0123@naver.com>
2020-08-31 01:09:35 +0900

    자물쇠와 열쇠 내 풀이

--- 카카오블라인드2020/3번_자물쇠와 열쇠.py
+++ 카카오블라인드2020/3번_자물쇠와 열쇠.py
@@ -1,3 +1,54 @@
+def attach(x, y, M, key, board):
+    for i in range(M):
+        for j in range(M):
+            board[x+i][y+j] += key[i][j]
+
+
+def detach(x, y, M, key, board):
+    for i in range(M):
+        for j in range(M):
+            board[x+i][y+j] -= key[i][j]
+
+
+def rotate90(arr):
+    return list(zip(*arr[::-1]))
+
+
+def check(board, M, N):
+    for i in range(N):
+        for j in range(N):
+            if board[M+i][M+j] != 1:
+                return False
+    return True
+
+
+def solution(key, lock):
+    M, N = len(key), len(lock)
+
+    board = [[0] * (M*2 + N) for _ in range(M*2 + N)]
+    # 자물쇠 중앙 배치
+    for i in range(N):
+        for j in range(N):
+            board[M+i][M+j] = lock[i][j]
+
+    rotated_key = key
+    # 모든 방향 (4번 루프)
+    for _ in range(4):
+        rotated_key = rotate90(rotated_key)
+        for x in range(1, M+N):
+            for y in range(1, M+N):
+                # 열쇠 넣어보기
+                attach(x, y, M, rotated_key, board)
+                # lock 가능 check
+                if(check(board, M, N)):
+                    return True
+                # 열쇠 빼기
+                detach(x, y, M, rotated_key, board)
+
+    return False
+
+
+'''
 # 다른 풀이 - 보드크기를 세배로
 def rotate90(a):
     n = len(a)
@@ -45,3 +96,4 @@ def solution(key, lock):
                         new_lock[x+i][y+j] -= key[i][j]
 
     return False
+'''
