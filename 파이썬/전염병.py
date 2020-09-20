80d578aefce8b5f70c3bf7cf3490b9cf880651d7
Um seoyoung <tjdud0123@naver.com>
2020-09-20 23:16:40 +0900

    0920

--- 파이썬/전염병.py
+++ 파이썬/전염병.py
@@ -1,37 +1,29 @@
-def visitable(x, y, m, n, state):
-    return 0 <= x < m and 0 <= y < n and state[x][y]
+def can_infest(y, x, m, n, state):
+    return 0 <= y < m and 0 <= x < n and state[y][x] == 0
 
+def solution(m, n, infests, vaccinateds):   
+    blank = m*n - len(infests) - len(vaccinateds)
+    if blank == 0: # 모두 백신이나 감염
+        return 0
+    
+    state = [[0] * n for i in range(m)] # 상태 확인 표
+    for y, x in vaccinateds + infests:
+        state[y-1][x-1] = 1
+        
+    day = -1
+    cur_infests = [(y-1, x-1) for y, x in infests]
+    
+    DELTAS, Y, X = [(0, 1), (0, -1), (-1, 0), (1, 0)], 0, 1
+    while cur_infests:
+        day += 1
+        new_infests = []
+        for infest in cur_infests:
+            for dy, dx in DELTAS:
+                nxt = infest[Y] + dy, infest[X] + dx
+                if can_infest(*nxt, m, n, state):
+                    new_infests.append(nxt)
+                    state[nxt[Y]][nxt[X]] = 1
+                    blank -= 1
+        cur_infests = new_infests
 
-def solution(m, n, infests, vaccinateds):
-
-    answer, whole, count = 0, m*n, 0
-    state = [[True] * n for _ in range(m)]
-    new_infe = []
-
-    for x, y in infests:
-        state[x-1][y-1] = False
-        count += 1
-        new_infe.append((x-1, y-1))
-
-    for x, y in vaccinateds:
-        state[x-1][y-1] = False
-        count += 1
-
-    Deltas = ((0, 1), (0, -1), (-1, 0), (1, 0))
-
-    while True:
-        if not new_infe:
-            return answer if count == whole else -1
-        temp = []
-        while new_infe:
-            cur_p = new_infe.pop()
-            for dx, dy in Deltas:
-                x, y = cur_p[0] + dx, cur_p[1] + dy
-                if visitable(x, y, m, n, state):
-                    state[x][y] = False
-                    count += 1
-                    if count == whole:
-                        return answer + 1
-                    temp.append((x, y))
-        answer += 1
-        new_infe += temp
+    return day if blank == 0 else -1
