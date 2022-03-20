0720f0f5c027393ec2724b9f36e9f3919778f524
Um seoyoung <tjdud0123@naver.com>
2022-03-21 02:59:01 +0900

    js 자주쓰는 문법 추가

--- 자주쓰는_문법.js
+++ 자주쓰는_문법.js
@@ -79,7 +79,7 @@ const byteNum = num.toString(2), powNum = Math.pow(2, num);
 // expected output: "1000 256"
 //--------------------------------------------//
 
-/* 잘라내기
+/* 잘라내기 - slice
  * like python [:]
  * 원본 배열은 그대로 두고 새로운 배열 반환
  * array & string
@@ -96,6 +96,14 @@ console.log(words.slice(4, 19)); // "quick brown fox"
 console.log(words.slice(-4)); // "dog."
 console.log(words.slice(-9, -5)); // "lazy"
 //--------------------------------------------//
+
+/** 제거 - splice
+ * like delete, 원본 배열 변형
+ * (인덱스, 요소 개수)
+ * 비어있는 배열 잘라내도 에러를 내진 않음
+ */
+console.log(animals.splice(2, 2)); // [ 'camel', 'duck' ]
+console.log(animals); // [ 'ant', 'bison', 'elephant' ]
 
 /* 정렬
  * 오름차순 / 내림차순
@@ -123,6 +131,7 @@ const word = 'abc'
 const doubleStr = word.repeat(2) // 'abcabc'
 
 let visited = Array.from(new Array(n), () => false) // like [false] * n
+let visited2 = Array(n).fill(false) // [false] * n
 
 const matrix = Array.from(Array(3), () => Array(2).fill(null))
 console.log(matrix) // 3 * 2 null로 세팅된 2차배열
@@ -132,8 +141,9 @@ for (const p of participant){
     table[p] = (table[p] || 0) + 1
 }
 
-let forRange = [...Array(5).keys()].map(x => x + 1)
-// [ 1, 2, 3, 4, 5 ]
+let forRange = [...Array(5).keys()].map(x => x + 1) // [ 1, 2, 3, 4, 5 ]
+let forRange2 = Array.from({length: 10}, (_, i) => i+1) // [ 1, 2, 3, ... ,10 ]
+
 //--------------------------------------------//
 
 /* stack & que
@@ -141,12 +151,52 @@ let forRange = [...Array(5).keys()].map(x => x + 1)
  * push, pop, shift
  */
 let data = ['a', 'b', 'c', 'd'];
+const top = data[data.length-1] || null
 
 data.push('e'); // ['a', 'b', 'c', 'd', 'e']
 const pop = data.pop(); // 'e'
+/* push - 2개도 가능 */
+data.push('1', '2'); // [ 'a', 'b', 'c', 'd', '1', '2' ]
 
 const popLeft = data.shift(); // 'a'
 data.unshift('a'); // // ['a', 'b', 'c', 'd']
+
+class Queue {
+    constructor() {
+        this.queue = [];
+        this.front = 0;
+        this.rear = 0;
+    }
+    push(value) {
+        this.queue[this.rear++] = value;
+    }
+    popleft() {
+        if (this.queue.length === 0) {
+            return null;
+        }
+        const value = this.queue[this.front];
+        delete this.queue[this.front];
+        this.front += 1;
+        return value;
+    }
+    peek() {
+        return this.queue[this.front]
+    }
+}
+
+const que = new Queue ();
+console.log(que) // Queue { queue: [], front: 0, rear: 0 }
+que.popleft()
+console.log(que)
+que.push(1)
+que.push(2)
+que.push(3)
+que.popleft()
+console.log(que.peek()) // 2
+console.log(que) // Queue { queue: [ <1 empty item>, 2, 3 ], front: 1, rear: 3 }
+console.log(que.popleft()) // 2
+console.log(que) // Queue { queue: [ <2 empty items>, 3 ], front: 2, rear: 3 }
+
 //--------------------------------------------//
 
 /* reduce
@@ -162,6 +212,10 @@ const flattened = doubleArr.reduce(( acc, cur ) => acc.concat(cur), []);
 const sumWithInitial = flattened.reduce((prev, cur, idx) => prev + cur + idx, 0);
 console.log(sumWithInitial) 
 // 0 + 0+0 + 1+1 + 2+2 + 3+3 + 4+4 + 5+5 => 30
+
+const sum = Array
+    .from(new Array(5), (_, i) => i + 1)
+    .reduce((acc, cur) => acc + cur, 0); // 1 + 2 + 3 + 4 + 5
 
 const names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice'];
 const countedNames = names.reduce((allNames, name) => {
@@ -196,6 +250,9 @@ console.log(Object.keys(obj)); // ["a", "b", "c"]
 console.log(Object.getOwnPropertyNames(obj)); // ["a", "b", "c"]
 
 console.log(obj.hasOwnProperty('a')); // true
+console.log('a' in obj); // true
+
+delete obj.a // 제거
 
 // 배열 확인은 includes
 // arr.includes('a');
@@ -216,6 +273,10 @@ var intersection = new Set([...set1].filter(x => set2.has(x)));
 
 // 차집합은 다음으로 흉내낼 수 있음
 var difference = new Set([...set1].filter(x => !set2.has(x)));
+
+// 중복요소 제거
+const repeated = ['Lee', 'Kim', 'Park', 'Lee', 'Kim'];
+const unique = [...new Set(repeated)]; //  [ 'Lee', 'Kim', 'Park' ]
 
 //--------------------------------------------//
 
@@ -387,5 +448,76 @@ const compressed = beforeCompress
     .match(compressRegExp)
     .reduce((acc, cur) => acc + `${cur.length}${cur.slice(-1)}`, "")
 console.log(compressed) // 6A3B1D7F2K
+
+//--------------------------------------------//
+
+/* 소수구하기
+ * 에라토스테네스의 체
+ */
+const MAX = 100
+let primeSet = new Set()
+
+// 소수 체크
+let primes = [false, false, ...Array(MAX-1).fill(true)]
+
+for (let j = 2; j <= MAX; j++) {
+    if (primes[j]) {
+        primeSet.add(j)
+        for (let k = 2 * j; k <= MAX; k += j) {
+            primes[k] = false
+        }
+    }
+}
+
+console.log([...primeSet]) // [ 2,  3,  5,  7, 11, 13, 17, 19, ...]
+
+//--------------------------------------------//
+
+/* swap
+ * ES6의 구조 분해 할당 문법을 사용하여 두 변수를 swap
+ */
+let a = 5, b = 10;
+[a, b] = [b, a];
+console.log(a, b); // 10 5
+
+//--------------------------------------------//
+
+/* 동적 속성 이름
+ * ES6에 추가된 기능으로 객체의 키를 동적으로 생성
+ */
+const nameKey = 'name';
+const emailKey = 'email';
+const person = {
+    [nameKey]: 'Usy',
+    [emailKey]: 'tjdud0123@naver.com'
+};
+console.log(person);
+
+// {
+//   name: 'Usy',
+//   email: 'tjdud0123@naver.com'
+// }
+
+//--------------------------------------------//
+
+/* !! 연산자를 사용하여 Boolean 값으로
+ * !! 연산자를 이용하여 0, null, 빈 문자열, undefined, NaN을 false로 그 외에는 true로 변경
+ */
+
+const check = (variable) => {
+    if (!!variable) {
+        console.log(variable);
+    } else {
+        console.log('잘못된 값');
+    }
+}
+  check(null); // 잘못된 값
+  check(3.14); // 3.14
+  check(undefined); // 잘못된 값
+  check(0); // 잘못된 값
+  check('Good'); // Good
+  check(''); // 잘못된 값
+  check(NaN); // 잘못된 값
+  check(5); // 5
 
 //--------------------------------------------//
