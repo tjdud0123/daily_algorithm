b5dd2b1f28eeae3a1df22042209adabdbe7732ec
Um seoyoung <tjdud0123@naver.com>
2022-04-14 01:52:34 +0900

    python map, string 제어, zip, split

---
+++ 파이썬/Encryption.py
@@ -0,0 +1,8 @@
+def encryption(s):
+    L = len(s)
+    COL_NUM = math.ceil(L**0.5)
+    splited = [s[i:i+COL_NUM] for i in range(0, L, COL_NUM)]
+    splited = [x + ' ' *(COL_NUM - len(x)) for x in splited]
+    arranged = [''.join(x).strip() for x in map(list, zip(*splited))]
+
+    return ' '.join(arranged)
