793019949a00694130cbe721c0103f5b9b08c2c0
Um seoyoung <tjdud0123@naver.com>
2022-04-16 23:14:32 +0900

    map 함수 추가

--- 파이썬/Encryption.py
+++ 파이썬/Encryption.py
@@ -3,6 +3,7 @@ def encryption(s):
     COL_NUM = math.ceil(L**0.5)
     splited = [s[i:i+COL_NUM] for i in range(0, L, COL_NUM)]
     splited = [x + ' ' *(COL_NUM - len(x)) for x in splited]
+    # splited = map(lambda x:x + ' ' *(COL_NUM - len(x)), splited)
     arranged = [''.join(x).strip() for x in map(list, zip(*splited))]
 
     return ' '.join(arranged)
