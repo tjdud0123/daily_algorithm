9571194f7205b2bae9fbeedec157566361bb7562
Um seoyoung <tjdud0123@naver.com>
2022-05-23 19:13:04 +0900

    최대최소 관련

--- 자바스크립트/maxPerimeter.js
+++ 자바스크립트/maxPerimeter.js
@@ -1,21 +1,21 @@
 // 넓이가 주어졌을 때 둘레의 최솟값
 
 function solution1(N) {
-    let minHalfPeri = Number.MAX_SAFE_INTEGER;
+    let minHalfPeri = Number.MAX_SAFE_GER;
     for (let i = 1; i <= Math.sqrt(N); i++) {
         if (N % 1 === 0) {
             const halfPeri = i + parseInt(N / i);
-            minHalfPeri = Math.min(halfPeri, minHalfPeri)
+            minHalfPeri = Math.min(halfPeri, minHalfPeri);
         }
     }
-    return minHalfPeri * 2
+    return minHalfPeri * 2;
 }
 
 function solution2(N) {
     let maxFactor = parseInt(Math.sqrt(N));
     for (let i = maxFactor; i >= 1; i--) {
         if (N % i === 0) {
-            return (i+parseInt(N / i)) * 2;
+            return (i + parseInt(N / i)) * 2;
         }
     }
 }
